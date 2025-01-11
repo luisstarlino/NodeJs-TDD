38d7754c56ac93ba14acacd5ea2a9b9a4b7ff3c3
Luis Starlino <luis.guilherme009@gmail.com>
2025-01-11 18:06:37 -0300

    refactor(orm): start to fix the current connection when i use dev or not

--- tests/posts.test.ts
+++ tests/posts.test.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import  createConnection from '../src/database/index';
 
 const server = axios.create({
-  baseURL: 'http://localhost:5001'
+  baseURL: 'http://localhost:5001/v1'
 })
 
 describe('/posts', () => {
-  it('Should return status 200 and list of players', async () => {
+  it('Should return status 200 and list of posts', async () => {
     const expectedResponse = [
       {
         post_id: 'b0f3d72a-b8a9-45ec-a8f0-a14f65f3597d',
@@ -19,8 +20,27 @@ describe('/posts', () => {
       }
     ]
 
-    const response = await server.get('/posts')
-    expect(response.status).toBe(200)
-    expect(response.data).toMatchObject(expectedResponse)
+    const response = await server.get('/posts');
+    expect(response.status).toBe(200);
+    expect(response.data).toMatchObject(expectedResponse);
+
   })
+
+  // it('Should return status 200 when post saved', async () => {
+  //   const response = await server.post('/post', {
+  //     "author": "luis@starlino.com",
+  //     "content": "integration test"
+  //   })
+
+  //   // TODO: Make the correct integration with the delete route!
+  //   const connection = await createConnection();
+  //   await connection.query(`DELETE FROM posts WHERE post_id = ${response.data.post_id}`);
+  //   await connection.close();
+
+  //   expect(response.status).toBe(201);
+  //   expect(response.data).toMatchObject({
+  //     "author": "luis@starlino.com",
+  //     "content": "integration test"
+  //   });
+  // })
 })
