ba502cc519a35ea9d4273cfc8d809107d245a4fa
Luis Starlino <luis.guilherme009@gmail.com>
2025-01-11 19:10:05 -0300

    add all

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './app'
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`🔵 Server on port ${process.env.PORT}`)
-  console.log(`🔵 http://localhost:${process.env.PORT}`);
-  
-})
